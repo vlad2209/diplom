d260103ec8d5bd71f64c53d684d95e8d3a89021e
AlinaKlishyna <alinakotova102@gmail.com>
2020-04-26 21:32:47 +0300

    Добавьте файлы проекта.

---
+++ TestBot/PreformCommand.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.IO;
+
+namespace Telegram
+{
+    namespace Request
+    {
+        #region DefaultClass
+        public class User
+        {
+            public int id;
+            public string first_name;
+            public string last_name;
+            public string username;
+        }
+        public class Chat
+        {
+            public int id;
+            public string type;
+            public string title;
+            public string username;
+            public string first_name;
+            public string last_name;
+
+        }
+        public class PhotoSize
+        {
+            public string file_id;
+            public int width;
+            public int height;
+            public int file_size;
+        }
+        #endregion
+        #region Делгаты
+        public delegate void ResponseText(object sendr, MessageText e);
+        public delegate void ResponsePhoto(object sendr, MessagePhoto e);
+        public delegate void ResponseLocation(object sendr, MessageLocation e);
+        public delegate void ResponseContact(object sendr, MessageContact e);
+        #endregion
+        #region Классы Типов
+        public class MessageText : EventArgs
+        {
+            #region DefaulParameters
+            public int message_id;
+            public User from = new User();
+            public Chat chat = new Chat();
+            public int date;
+            #endregion
+            public string text;
+        }
+
+        public class MessagePhoto : EventArgs
+        {
+            #region DefaulParameters
+            public int message_id;
+            public User from = new User();
+            public Chat chat = new Chat();
+            public int date;
+            #endregion
+            public List<PhotoSize> photo = new List<PhotoSize>();
+            public string caption;
+        }
+        public class MessageLocation : EventArgs
+        {
+            #region DefaulParameters
+            public int message_id;
+            public User from = new User();
+            public Chat chat = new Chat();
+            public int date;
+            #endregion
+            public float longitude;
+            public float latitude;
+        }
+        public class MessageContact : EventArgs
+        {
+            #region DefaulParameters
+            public int message_id;
+            public User from = new User();
+            public Chat chat = new Chat();
+            public int date;
+            #endregion
+            public string phone_number;
+            public string first_name;
+            public string last_name;
+            public int user_id;
+        }
+
+
+        public class TelegramRequest
+        {
+            public string _token;
+            public TelegramRequest(string Token)
+            {
+                _token = Token;
+            }
+
+        }
+    }
+
+
+    class Method
+    {
+        string _token;
+        string LINK = "https://api.telegram.org/bot";
+        public Method(string Token)
+        {
+            _token = Token;
+        }
+        public string Getme()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string response = webClient.DownloadString(LINK + _token + "/getMe");
+                return response;
+            }
+        }
+
+        public void ForwardMessage(int fromChatID, int chatId, int messageID)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                NameValueCollection pars = new NameValueCollection();
+                pars.Add("chat_id", chatId.ToString());
+                pars.Add("from_chat_id", fromChatID.ToString());
+                pars.Add("message_id", messageID.ToString());
+                webClient.UploadValues(LINK + _token + "/forwardMessage", pars);
+            }
+        }
+
+        async public Task SendPhotoIputFile(int ChatID, string pathToPhoto, string catprion = "")
+        {
+            using (MultipartFormDataContent form = new MultipartFormDataContent())
+            {
+                string url = LINK + _token + "/sendPhoto";
+                string fileName = pathToPhoto.Split('\\').Last();
+
+                form.Add(new StringContent(ChatID.ToString(), Encoding.UTF8), "chat_id");
+                form.Add(new StringContent(catprion.ToString(), Encoding.UTF8), "caption");
+                using (FileStream fileStream = new FileStream(pathToPhoto, FileMode.Open, FileAccess.Read))
+                {
+                    form.Add(new StreamContent(fileStream), "photo", fileName);
+                    using (HttpClient client = new HttpClient())
+                        await client.PostAsync(url, form);
+                }
+            }
+
+        }
+    }
+}
+
+
+         #endregion
---
+++ TestBot/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+using Telegram.Request;
+using Telegram;
+
+
+
+ 
+
+namespace TestBot
+{
+    class Program 
+    {
+        static TelegramBotClient Bot;
+        static ApiAi apiAi;
+
+        static void Main(string[] args)
+        {
+
+            Bot = new TelegramBotClient("792016498:AAEC2bDyfRLe2mdlK28Wc6GzccPg52oW_L0");
+            AIConfiguration config = new AIConfiguration("890fc148d46046fe9ec0716db01c751e", SupportedLanguage.Russian);
+            apiAi = new ApiAi(config);
+
+
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallBackQueryReceived;
+
+
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.WriteLine(me.FirstName);
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StartReceiving();
+        }
+
+        private static async void BotOnCallBackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+        {
+            string buttonText = e.CallbackQuery.Data;
+            string name = $"{e.CallbackQuery.From.FirstName} {e.CallbackQuery.From.LastName}";
+            Console.WriteLine($"{name} нажал кнопку {buttonText}");
+
+            try
+            {
+
+                if (buttonText == "Замены")
+                {
+                   Message x = await Bot.SendTextMessageAsync(e.CallbackQuery.Id, "http://kipt.sumdu.edu.ua/zaninu-na-zavtra.html");
+                }
+                else if (buttonText == "ИптКиСумДу")
+                {
+                   Message x = await Bot.SendTextMessageAsync(e.CallbackQuery.Id, "https://www.youtube.com/watch?v=mj4tAgzhE7E");
+                }
+                await Bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"Вы нажали конпку {buttonText}");
+            }
+            catch (Exception ex)
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            
+
+        }
+
+        private static async void BotOnMessageReceived(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+
+
+            if (message == null || message.Type !=MessageType.Text)
+                return; 
+            string name = $"{message.From.FirstName} {message.From.LastName}";
+
+
+            Console.WriteLine($"{name} отправил сообщение: '{message.Text}'");
+
+            switch (message.Text)
+            {
+                case "/start":
+                    string text =
+                        @"Вот список команд которыми ты можешь пользоватся
+/inline - вывод меню
+/keyboard - вывод клавиатуры
+/Напиши мне 'Привет' ";
+
+                    await Bot.SendTextMessageAsync(message.From.Id, text);
+                    break;
+
+                case "/inline":
+                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithUrl("Замены","http://kipt.sumdu.edu.ua/zaninu-na-zavtra.html"),
+                            InlineKeyboardButton.WithUrl("VK","https://vk.com/id133155332"),
+                            InlineKeyboardButton.WithUrl("Погода","https://sinoptik.ua/погода-конотоп-303011665 "),
+                            InlineKeyboardButton.WithUrl("Рассписание Поездов ","http://poezdato.net/raspisanie-poezdov/bahmach--konotop/elektrichki/")
+
+                        },
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData("731"),
+                            InlineKeyboardButton.WithCallbackData("ИптКиСумДу")
+                            
+                        }
+
+                    });
+                    await Bot.SendTextMessageAsync(message.From.Id, "Выберите пункт меню",
+                        replyMarkup: inlineKeyboard);
+                    break;
+
+                case "/keyboard":
+                    var replyKeyboard = new ReplyKeyboardMarkup(new[]
+                    {
+                        new[]
+                        {
+                            new KeyboardButton("Привет"),
+                            new KeyboardButton("Номери Телефонів"),
+                            new KeyboardButton("Електронна пошта Вчителів")
+                        },
+                        new []
+                        {
+                            new KeyboardButton("Геолокация") {RequestLocation = true }
+
+                        }
+                    });
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "Сообщение",
+                        replyMarkup: replyKeyboard);
+                    break;
+
+                default:
+                    var response = apiAi.TextRequest(message.Text);
+                    string answer = response.Result.Fulfillment.Speech;
+                    if ( answer == "")
+
+                       answer = "Прости, я тебя не понял";
+                    await Bot.SendTextMessageAsync(message.From.Id, answer);
+                    
+
+                    break;
+                   
+
+
+            }
+           
+
+        }
+
+    }
+}
